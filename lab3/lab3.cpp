f41b5d565191a71f5a2372cb3d3b32b9e52151bc
igoridze.reap@gmail.com <igoridze.reap@gmail.com>
2020-03-28 16:36:33 +0200

    Добавьте файлы проекта.

---
+++ lab3/lab3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+//1.Дан массив символов. Удалить символ двоеточие и подсчитать количество удаленных символов.
+
+
+void ex1() 
+{
+    string strk;
+    getline(cin, strk);
+
+    cout << "Строка: " << strk << endl;
+
+    int count = 0;
+    while (strk.find(":") != -1)
+    {
+        count++;
+        strk.erase(strk.begin() + strk.find(":"));// от начала строки ищем дветочие и удаляем.
+    }
+
+    cout << "Удалили символов: " << count << endl;
+    cout << "Отформатированная строка: " << strk << endl;
+}
+int main()
+{
+    setlocale(0, "rus");
+    ex1();
+}
